17a5274b866a7bc6fdacfd73f83fa47c5792c8f3
John Wallace <wallace.john.w@gmail.com>
2026-08-30 13:53:43 -0400

    v0.1.3 (#20)

--- src/app/app_identity.h
+++ src/app/app_identity.h
@@ -16,6 +16,6 @@ namespace zc {
 #endif
 
 inline const char* kDefaultPublicAppKey = ZCOMMS_APP_KEY;
-inline const char* kAppVersion = "0.1.2";
+inline const char* kAppVersion = "0.1.3";
 
 }  // namespace zc
